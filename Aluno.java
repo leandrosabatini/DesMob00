0d558b90bd3eaa91a98b570844c3fc045e08c993
Leandro Sabatini <contato@leandrosabatini.com.br>
2020-03-24 20:50:38 -0300

    Criando contrutor da classe Aluno

--- Aluno.java
+++ Aluno.java
@@ -2,6 +2,11 @@ public class Aluno{
     private int id;
     private String nome;
 
+    public Aluno(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
     public int getId() {
         return id;
     }
