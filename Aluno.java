327a1425bc3732cce1887448b0c96d96f09ded98
Gabriel Santos <Gabriel.jeronimo1997@hotmail.com>
2020-03-24 20:44:37 -0300

    Criando setters da classe Aluno

--- Aluno.java
+++ Aluno.java
@@ -6,7 +6,15 @@ public class Aluno{
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 }
