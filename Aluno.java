931eb7bd784fc81561e54b18ca29b17442dc9d89
Leandro Sabatini <contato@leandrosabatini.com.br>
2020-03-24 20:34:20 -0300

    Criando getters da classe Aluno

--- Aluno.java
+++ Aluno.java
@@ -1,4 +1,12 @@
 public class Aluno{
     private int id;
     private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
